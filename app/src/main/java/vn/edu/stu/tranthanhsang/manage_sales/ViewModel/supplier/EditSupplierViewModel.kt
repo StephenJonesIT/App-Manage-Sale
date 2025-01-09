9e8e5cf70c2cf4799d901a7a2980e4bb5784baa8
Thanh Sang <tranthanhsang.it.la@gmail.com>
2025-01-09 21:05:15 +0700

    feat: Update supplier exitest

--- app/src/main/java/vn/edu/stu/tranthanhsang/manage_sales/ViewModel/supplier/EditSupplierViewModel.kt
+++ app/src/main/java/vn/edu/stu/tranthanhsang/manage_sales/ViewModel/supplier/EditSupplierViewModel.kt
@@ -1,10 +1,101 @@
 package vn.edu.stu.tranthanhsang.manage_sales.viewModel.supplier
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import vn.edu.stu.tranthanhsang.manage_sales.data.model.common.StatusResponse
+import vn.edu.stu.tranthanhsang.manage_sales.data.model.supplier.UpdateSupplierRequest
 import vn.edu.stu.tranthanhsang.manage_sales.data.repository.SupplierRepository
+import vn.edu.stu.tranthanhsang.manage_sales.utils.StringUtils
 
 class EditSupplierViewModel(
     private val supplierRepository: SupplierRepository
 ) : ViewModel() {
+    private val _isStatusEdit = MutableLiveData<Boolean>()
+    private val _isConfirmClicked = MutableLiveData<Boolean>()
+    private val _isStatusDelete = MutableLiveData<Result<StatusResponse>>()
+    private val _isStatusUpdate = MutableLiveData<Result<StatusResponse>>()
 
+    val isStatusEdit: LiveData<Boolean> get() = _isStatusEdit
+    val isConfirm: LiveData<Boolean> get() = _isConfirmClicked
+    val isStatusDelete: LiveData<Result<StatusResponse>> get() = _isStatusDelete
+    val isStatusUpdate: LiveData<Result<StatusResponse>> get() = _isStatusUpdate
+
+    private val _idSupplier = MutableLiveData<String>()
+    private val _nameSupplier = MutableLiveData<String>()
+    private val _telephone = MutableLiveData<String>()
+    private val _address = MutableLiveData<String>()
+    private val _supplierType = MutableLiveData<String>()
+
+
+    val idSupplier: LiveData<String> get() = _idSupplier
+    val nameSupplier: LiveData<String> get() = _nameSupplier
+    val telephone: LiveData<String> get() = _telephone
+    val address: LiveData<String> get() = _address
+    val supplierType: LiveData<String> get() = _supplierType
+
+    fun setIdSupplier(id: String) {
+        _idSupplier.value = id
+    }
+    fun setNameSupplier(name: String) {
+        _nameSupplier.value = name
+    }
+    fun setTelephone(telephone: String) {
+        _telephone.value = telephone
+    }
+    fun setAddress(address: String) {
+        _address.value = address
+    }
+    fun setSupplierType(type: String) {
+        _supplierType.value = type
+    }
+    init {
+        _isStatusEdit.value = false
+    }
+    fun onStatusEditClicked() {
+        _isStatusEdit.value = true
+    }
+
+    fun onConfirmClicked() {
+        _isConfirmClicked.value = true
+    }
+
+    fun setToken(newToken: String){
+        supplierRepository.updateToken(newToken)
+    }
+    fun resetStatusEdit(){
+        _isStatusEdit.value = false
+    }
+
+    fun updateSupplier(){
+        val idSupplier = _idSupplier.value
+        val nameSupplier = _nameSupplier.value
+        val (Ho, Ten) = StringUtils.splitFullName(nameSupplier.toString())
+        val telephone = _telephone.value
+        val address = _address.value
+        val supplierType = _supplierType.value
+        viewModelScope.launch {
+            val result = supplierRepository.updateSupplier(
+                idSupplier.toString(),
+                UpdateSupplierRequest(
+                    Ho,
+                    Ten,
+                    telephone.toString(),
+                    address.toString(),
+                    supplierType?.toInt()?:3
+                )
+            )
+            _isStatusUpdate.value = result
+        }
+    }
+
+    fun deleteSupplier(){
+        val idSupplier = _idSupplier.value
+        viewModelScope.launch {
+            val result = supplierRepository.deleteSupplier(idSupplier.toString())
+            _isStatusDelete.value = result
+        }
+    }
 }
--- app/src/main/java/vn/edu/stu/tranthanhsang/manage_sales/ui/supplier/SupplierEditActivity.kt
+++ app/src/main/java/vn/edu/stu/tranthanhsang/manage_sales/ui/supplier/SupplierEditActivity.kt
@@ -1,21 +1,196 @@
 package vn.edu.stu.tranthanhsang.manage_sales.ui.supplier
 
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import vn.edu.stu.tranthanhsang.manage_sales.R
+import vn.edu.stu.tranthanhsang.manage_sales.data.model.products.Product
+import vn.edu.stu.tranthanhsang.manage_sales.data.model.supplier.Supplier
+import vn.edu.stu.tranthanhsang.manage_sales.data.repository.SupplierRepository
+import vn.edu.stu.tranthanhsang.manage_sales.databinding.ActivitySupplierEditBinding
+import vn.edu.stu.tranthanhsang.manage_sales.databinding.CustomDialogBinding
+import vn.edu.stu.tranthanhsang.manage_sales.utils.PriceUtils
+import vn.edu.stu.tranthanhsang.manage_sales.utils.ToastUtils
+import vn.edu.stu.tranthanhsang.manage_sales.utils.TokenManage
+import vn.edu.stu.tranthanhsang.manage_sales.viewModel.products.EditProductViewModelFactory
+import vn.edu.stu.tranthanhsang.manage_sales.viewModel.supplier.EditSupplierViewModel
+import vn.edu.stu.tranthanhsang.manage_sales.viewModel.supplier.EditSupplierViewModelFactory
 
 class SupplierEditActivity : AppCompatActivity() {
+    private lateinit var editSupplier: EditSupplierViewModel
+    private lateinit var binding: ActivitySupplierEditBinding
+    private lateinit var tokenManager: TokenManage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_supplier_edit)
+
+        val repository = SupplierRepository()
+        editSupplier = ViewModelProvider(
+            this,
+            EditSupplierViewModelFactory(repository)
+            )[EditSupplierViewModel::class.java]
+
+        tokenManager = TokenManage(this)
+        editSupplier.setToken(tokenManager.getToken().toString())
+        binding.editSupplier = editSupplier
+        binding.lifecycleOwner = this
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_supplier_edit)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left/2, systemBars.top/2, systemBars.right/2, systemBars.bottom/2)
             insets
+        }
+        addEvents()
+        observeEvents()
+        getDataFromIntent()
+    }
+
+    private fun observeEvents() {
+        editSupplier.isStatusEdit.observe(this){
+            if(it){
+                setMessageTitle("Sửa nhà cung cấp")
+                setStatusEvents(View.INVISIBLE, View.VISIBLE)
+                setEnabledEditText(true)
+                setColorEditText("#FF000000")
+            }else{
+                setMessageTitle("Chi tiết nhà cung cấp")
+                setStatusEvents(View.VISIBLE,View.INVISIBLE)
+                setEnabledEditText(false)
+                setColorEditText("#797979")
+            }
+        }
+        editSupplier.isStatusUpdate.observe(this){
+            if(it.isSuccess){
+                ToastUtils.showToast(this,"Sửa nhà cung cấp thành công")
+                startActivity(Intent(this,SupplierListActivity::class.java))
+            }else{
+                ToastUtils.showToast(this,"Sửa nhà cung cấp thất bại")
+            }
+        }
+        editSupplier.isConfirm.observe(this){
+            if(it){
+                setValueViewModel()
+                editSupplier.updateSupplier()
+            }
+        }
+        editSupplier.isStatusDelete.observe(this){
+            if(it.isSuccess){
+                ToastUtils.showToast(this,"Ngừng hợp tác nhà cung cấp thành công")
+                startActivity(Intent(this,SupplierListActivity::class.java))
+            }else{
+                ToastUtils.showToast(this,"Ngừng hợp tác nhà cung cấp thất bại")
+            }
+        }
+    }
+
+    private fun addEvents() {
+        binding.imgBack.setOnClickListener {
+            startActivity(Intent(this,SupplierListActivity::class.java))
+        }
+        binding.imgCancel.setOnClickListener {
+            showDialogOption("Hủy sửa nhà cung cấp","Bạn có muốn dừng việc sửa thông tin nhà cung cấp không?")
+        }
+        binding.btnDelete.setOnClickListener {
+            showDialogOption("Ngừng hợp tác","Bạn có muốn dừng việc hợp tác với nhà cung cấp này không?")
+        }
+    }
+    private fun showDialogOption(title: String,message: String) {
+        val dialogAlert = AlertDialog.Builder(this)
+        val binding: CustomDialogBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.custom_dialog,
+            null,
+            false
+        )
+
+        dialogAlert.setView(binding.root)
+        val dialog = dialogAlert.create()
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
+        binding.tvExit.setOnClickListener {
+            dialog.dismiss()
+        }
+        binding.tvConfirm.setOnClickListener {
+            when(title){
+                "Ngừng hợp tác" -> {
+                    editSupplier.deleteSupplier()
+                }
+                "Hủy sửa nhà cung cấp" -> {
+                    editSupplier.resetStatusEdit()
+                }
+            }
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    private fun setStatusEvents(visible: Int, invisible: Int) {
+        binding.imgEdit.visibility = visible
+        binding.imgBack.visibility = visible
+        binding.btnDelete.visibility = visible
+        binding.imgCancel.visibility = invisible
+        binding.imgOk.visibility = invisible
+        binding.btnLuu.visibility = invisible
+    }
+    private fun setMessageTitle(message: String) {
+        binding.tvTitle.text = message
+    }
+    private fun setEnabledEditText(status: Boolean) {
+        binding.edtIdSupplier.isEnabled = false
+        binding.edtNameSupplier.isEnabled = status
+        binding.edtTelephone.isEnabled = status
+        binding.edtAddress.isEnabled = status
+        binding.edtType.isEnabled = status
+    }
+
+    private fun setColorEditText(color: String) {
+        binding.edtIdSupplier.setTextColor(Color.parseColor("#797979"))
+        binding.edtNameSupplier.setTextColor(Color.parseColor(color))
+        binding.edtTelephone.setTextColor(Color.parseColor(color))
+        binding.edtAddress.setTextColor(Color.parseColor(color))
+        binding.edtType.setTextColor(Color.parseColor(color))
+    }
+
+    private fun setValueViewModel() {
+        val id = binding.edtIdSupplier.text.toString()
+        val name = binding.edtNameSupplier.text.toString()
+        val telephone = binding.edtTelephone.text.toString()
+        val address = binding.edtAddress.text.toString()
+        val typeSupplier = binding.edtType.text.toString()
+
+        editSupplier.setIdSupplier(id)
+        editSupplier.setNameSupplier(name)
+        editSupplier.setTelephone(telephone)
+        editSupplier.setAddress(address)
+        editSupplier.setSupplierType(typeSupplier)
+    }
+    private fun getDataFromIntent() {
+        val supplier = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("supplier", Supplier::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra("supplier")
+        }
+        supplier.let {
+            editSupplier.setIdSupplier(it?.MaNCC ?: "")
+            editSupplier.setNameSupplier("${it?.Ho}"+" ${it?.Ten}")
+            editSupplier.setTelephone(it?.SDT ?: "")
+            editSupplier.setAddress(it?.DiaChi ?: "")
+            editSupplier.setSupplierType(it?.LoaiNCC.toString())
         }
     }
 }
